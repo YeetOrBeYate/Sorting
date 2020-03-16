6a1b5412db35456b90c6071e69484dc9a2255d58
Kyle Corbin <kylecorbin26@gmail.com>
2020-03-16 19:35:34 -0400

    Got the bubble sort working

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -2,28 +2,42 @@
 
 yeet = [6,9,7,21,63,4,1]
 
-arr = [1,3,2,4]
+arr = [1,5,3,4,2]
 
 
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
+
         smallest_index = i
+
+
+        print('-------------')
         print(f"ITERATION-position:{i} what is the smallest number: {arr[smallest_index]}")
         # Making the given assumption that our first item is our smallest
 
         # Looping through the remaining right items of our assumed smallest
         for x in range(smallest_index+1, len(arr)):
+
+            print(f"looping through indexes: {smallest_index+1}-{len(arr)}")
+
             if arr[x] < arr[smallest_index]:
+
                 print(f"COMPARE-{arr[x]} is smaller than {arr[smallest_index]}")
+
                 smallest_index = x
+
+                print(f"TRUTH-The true smallests number:{arr[smallest_index]}")
              
         # TO-DO: swap
         # tuple swap
         # basically make the true smallest number swap values, and therefore positions, with the assumed smallest value
         print(f"SWAP-:{arr[smallest_index]} will replace :{arr[i]}")
         print(f"SWAP-:{arr[i]} will replace :{arr[smallest_index]}")
+
+        
         arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
+
 
 
     return arr
@@ -31,10 +45,24 @@ def selection_sort( arr ):
 print(selection_sort(yeet))
 
 
+# arr = [1,5,3,4,2]
+# arr = [1,3,5,4,2]
+
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
 
+    for x in range(0, len(arr)-1):
+
+        for j in range(0, len(arr)-1):
+            #when left if bigger than right
+            if arr[j] > arr[j+1]:
+                #put the smaller at the beginning of the array
+                #since this parent loop goes through each parent element we can just change the array using this child loops indexes
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
     return arr
+
+# print(bubble_sort(arr))
 
 
 # STRETCH: implement the Count Sort function below
